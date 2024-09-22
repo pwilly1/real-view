fc6d00e968944efadb722503a3889f838eea3c01
pwilly1 <prwillis2@gmail.com>
2024-09-22 17:43:19 -0400

    l

---
+++ projects/part4/script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const nav = document.querySelector('nav ul');
+    if (nav.style.display === "flex") {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "flex";
+    }
+}
